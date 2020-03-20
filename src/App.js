29a6b4393260ce9f35673fc067fbe9cc5d1a189a
Jewell <jhuffman815@gmail.com>
2020-03-20 18:36:36 -0400

    Modified App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import TodoItem from "./components/TodoItem"
+import todosData from "./components/todosData.js"
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: todosData
+    }
+  }
+
+  render() {
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} /> )
+    return (
+      <div className="todo-list">
+        {todoItems}
+      </div>
+    );
+  }
+  
+}
+
+export default App;
