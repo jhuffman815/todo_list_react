3437a6194af601a43591d4f230b210d7e474e53b
Jewell <jhuffman815@gmail.com>
2020-03-22 20:27:37 -0400

    Added styling

--- src/App.js
+++ src/App.js
@@ -9,17 +9,36 @@ class App extends React.Component {
     this.state = {
       todos: todosData
     }
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  render() {
-    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} /> )
-    return (
+  handleChange(id) {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+        return todo
+      })
+      return {
+        todos: updatedTodos
+      }
+    })
+  }
+
+
+render() {
+  const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
+
+  return (
       <div className="todo-list">
         {todoItems}
       </div>
-    );
+    )
   }
-  
-}
 
+}
 export default App;
